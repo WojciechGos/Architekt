ed568baa50d046febc57ddc608fba7d0f5dd6446
wojtek <wojtekgg93@gmail.com>
2020-03-11 07:18:29 +0000

    graph is working

--- main.cpp
+++ main.cpp
@@ -15,38 +15,40 @@ struct Edge{
 	}
 };
 
-// ostream & operator << (ostream& os, vector<pair<int,int>> vpair){
 
-// 	for(auto it : vpair){
-// 		os << "{" << it.first << " " << it.second << "}";
-// 	}
-
-// 	return os;
-// }
-
-void findPath(vector<pair<int, int>> adjacentList[], int source, int destinity, int weight);
-
-void addEdgeDirectedWithWeight(map<int, vector<Edge>> adjacentList, int source, int destinity, int weight){
-	adjacentList[source].push_back(Edge(destinity, weight));
-	cout << "addEdge: ";
+void printGraph(map<int, vector<Edge>>& adjacentyList, int Vertices){
 	
-	// for(const auto& it: adjacentList[source]){
-	// 	cout << source << " ";
-	// 	cout << it.first << " " << it.second << endl;
-	// }
-	
-}
 
-
-
-void printGraph(map<int, vector<Edge>> adjacentyList, int Vertices){
-	
-	cout << "Print Graph" << endl;
 	for(auto it : adjacentyList){
 		for(auto vec : it.second){
 			cout << "Edge from node " << it.first << " to node " << vec.destinity << " with weight " << vec.weight << endl;
 		}
 	}
+	
+}
+
+bool findPath(map<int,vector<Edge>> & adjacentList, int source, int destinity, int weight){
+	auto searchQueue = adjacentList[source];
+	
+	while (!searchQueue.empty())
+	{
+		auto edge = searchQueue.back();
+		if(edge.destinity == destinity){
+			cout << "road exist" << endl;
+			return true;
+		}
+		else{
+			searchQueue.pop_back();
+			searchQueue += adjacentList[edge];
+
+		}
+	}
+	return false;
+}
+
+
+void addEdgeDirectedWithWeight(map<int, vector<Edge>>& adjacentList, int source, int destinity, int weight){
+	adjacentList[source].push_back(Edge(destinity, weight));
 	
 }
 
@@ -61,7 +63,7 @@ int main(){
 
     for(int i=0; i < Z; i++){
 		cin >> firstBuild >> secondBuild >> high;
-		cout << "The egde between " << firstBuild << " and " << secondBuild << " with weight " << high << endl;
+		
 
 
 		addEdgeDirectedWithWeight(edges, firstBuild, secondBuild, high);
@@ -76,10 +78,5 @@ int main(){
 
 
 
-void findPath(vector<pair<int, int>> adjacentList[], int source, int destinity, int weight){
 
-	
-
-	
-}
 
