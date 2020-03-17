3e84ae3bc429f1770813528fd666766514e910cd
wojtek <wojtekgg93@gmail.com>
2020-03-17 11:56:46 +0000

    FindPath function working

--- main.cpp
+++ main.cpp
@@ -18,15 +18,25 @@ struct Edge{
 };
 
 
-void printAcjacent(const vector<Edge>& adjacentList, int source){
+void printAdjacent(const vector<Edge>& adjacentList, int source){
+	cout << endl;
 	for(auto it: adjacentList){
 		cout << "Edge{ " << source << " " << it.destinity << "} " << it.weight << endl;
 	}
 }
 
+bool isNotIn(int destinationNode, const vector<int>& processed){
 
+	for(auto it : processed){
+		if(it == destinationNode){
+			return true;
+		}
+	}
 
-void printGraph(map<int, vector<Edge>>& adjacentyList, int Vertices){
+	return false;
+}
+
+void printGraph(const map<int, vector<Edge>>& adjacentyList){
 	
 
 	for(auto it : adjacentyList){
@@ -37,25 +47,39 @@ void printGraph(map<int, vector<Edge>>& adjacentyList, int Vertices){
 	
 }
 
-bool findPath(map<int,vector<Edge>> & adjacentList, int source, int destinity, int weight){
-	cout << "Find Path: start" << endl;
+int findLowestCostNode(const vector<Edge>& edges){
+	int low = 999999;
+	for(auto it: edges){
+		if(low > it.weight){
+			low = it.weight;
+		}
+	}
+	return low;
+}
+
+bool findPath(map<int,vector<Edge>>& adjacentList, const int& source, const int& destinity, const int& weight, vector<Edge>& road){
+
 	auto searchQueue = adjacentList[source];
+	vector<int> processed;
+	
 
-
-	printAcjacent(adjacentList[source], source);
+    // printAdjacent(adjacentList[source], source);
 	
 	while (!searchQueue.empty())
 	{
-		cout << "searchQueue.front() - " << searchQueue.front().destinity << endl;
-	
+		cout << "searchQueue.front() - " << searchQueue.front().destinity << " weight=" <<  searchQueue.front().weight << endl;
+
+		road.push_back(searchQueue.front());
+
+		processed.push_back(searchQueue.front().destinity);
 		auto edge = searchQueue.front();
 		searchQueue.erase(searchQueue.begin());
-		if(edge.destinity == destinity){
-			cout << "road exist" << endl;
+
+		if(edge.destinity == destinity && isNotIn(edge.destinity, processed) ){
 			return true;
 		}
 		else{
-			
+
 			searchQueue.insert(searchQueue.end(), adjacentList[edge.destinity].begin(), adjacentList[edge.destinity].end());
 		}
 	}
@@ -65,8 +89,10 @@ bool findPath(map<int,vector<Edge>> & adjacentList, int source, int destinity, i
 
 void addEdgeDirectedWithWeight(map<int, vector<Edge>>& adjacentList, int source, int destinity, int weight){
 	adjacentList[source].push_back(Edge(destinity, weight));
-	
+	weight = -1*weight;
+	adjacentList[destinity].push_back(Edge(source, weight));
 }
+
 
 int main(){
 
@@ -76,23 +102,34 @@ int main(){
 
     int firstBuild, secondBuild, high;
 	map<int, vector<Edge>> edges;
+	vector<Edge> road;
+	int roadCost = 0;
 
     for(int i=0; i < Z; i++){
 		cin >> firstBuild >> secondBuild >> high;
+		cout << endl;
+		cout << "FirstBuild=" << firstBuild << " SecondBuild=" << secondBuild << " Weight=" << high << endl;
+		cout << endl;
+		printGraph(edges);
+
+		if(findPath(edges,firstBuild, secondBuild, high, road)){
+			for(auto it: road){
+				roadCost += it.weight;
+			}
+			if(roadCost == high){
+				addEdgeDirectedWithWeight(edges, firstBuild, secondBuild, high);
+			}
+		}else{
+			addEdgeDirectedWithWeight(edges, firstBuild, secondBuild, high);
+		}
 		
-
-
-		addEdgeDirectedWithWeight(edges, firstBuild, secondBuild, high);
+		road.clear();
 		
 	
     }
 
-	printGraph(edges, N);
-	if(findPath(edges, 4, 1, 0)){
-		cout << "Yes" << endl;
-	}else{
-		cout << "No " << endl;
-	}
+	
+	
 
     return 0;
 }
